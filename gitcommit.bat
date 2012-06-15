6bf8058f82654df26b8d2f7f9dbbbe8d99d4efdb
RGLoader <saundzz@hotmail.co.uk>
2012-06-15 23:56:42 +0100

    'added: gitcommit.bat, haven't tested properly but it should work'

---
+++ gitcommit.bat
@@ -0,0 +1,39 @@
+@echo off
+
+REM you should have git added to your %PATH% variable!
+
+setlocal EnableDelayedExpansion
+echo make sure VS is closed before running!
+SET /p desc=Enter a description of your changes: 
+SET /p yn=Really commit changes? (Y/n) [n]: 
+if (%yn%)==() ( SET yn=n )
+
+set no=false
+
+if "%yn%"=="n" ( SET no=true )
+if "%yn%"=="N" ( SET no=true )
+if "%yn%"=="no" ( SET no=true )
+if "%yn%"=="No" ( SET no=true )
+
+if "%no%"=="false" ( 
+echo cleaning files..
+
+rem remove build stuff 
+rem we only want to commit source
+for /D %%y in (*Resharper.*) do rmdir %%y /S /Q
+for /D %%z in (*.*) do rmdir %%z\bin /S /Q
+for /D %%x in (*.*) do rmdir %%x\obj /S /Q
+del /Q *.suo
+del /Q /S *.user
+
+echo commiting changes..
+git add *
+
+if ["%desc%"]==[] (
+git commit
+) else (
+git commit -m '%desc%'
+)
+
+git push -u origin master
+)
